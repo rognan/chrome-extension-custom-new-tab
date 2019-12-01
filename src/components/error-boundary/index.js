92b0db42dbb5fae86f07f2b7665857bb3148f844
Thor Andreas Rognan <thor.rognan@gmail.com>
2019-12-01 20:18:08 +0100

    Upgrade raven-js to @sentry/browser

--- src/components/error-boundary/index.js
+++ src/components/error-boundary/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Raven from 'raven-js';
+import { init, captureException, lastEventId, showReportDialog } from '@sentry/browser';
 import styles from './index.less';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {hasError: false};
-    Raven.config('https://ae0abaf060a842dd8996a744473193c3@sentry.io/227131').install();
+    init({
+        dsn: 'https://ae0abaf060a842dd8996a744473193c3@sentry.io/227131'
+    });
   }
   componentDidCatch(error, info) {
     this.setState({hasError: true});
-    Raven.captureException(error, {extra: info});
+    captureException(error, {extra: info});
   }
   render() {
     if (this.state.hasError) {
       return (
-        <div onClick={() => Raven.lastEventId() && Raven.showReportDialog()}>
+        <div onClick={() => lastEventId() && showReportDialog()}>
           <p>Whoops - Something went wrong!</p>
           <p className={styles.error__clickme}>Click here to fill out a bug report to help us fix this problem faster.</p>
         </div>
