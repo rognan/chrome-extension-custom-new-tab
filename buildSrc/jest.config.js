510ddf1b17eaa000b6471fad1d1e17407724960e
Thor Andreas Rognan <thor.rognan@gmail.com>
2021-06-12 16:36:20 +0200

    Delete unnecessary jest style mock

--- buildSrc/jest.config.js
+++ buildSrc/jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   rootDir: '..',
   moduleDirectories: ['node_modules', 'src'],
-  moduleNameMapper: {
-    '\\.(css)$': '<rootDir>/buildSrc/__mocks__/styleMock.js'
-  },
   setupFilesAfterEnv: ['<rootDir>/buildSrc/setupTest.js'],
   testEnvironment: 'jsdom'
 };
