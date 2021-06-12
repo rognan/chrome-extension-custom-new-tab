50bcb7d2dcf7ecc607c5d6c32355738be9c2e33a
Thor Andreas Rognan <thor.rognan@gmail.com>
2021-06-12 13:05:13 +0200

    Remove unused module name mapper

--- buildSrc/jest.config.js
+++ buildSrc/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   rootDir: '..',
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
-    '@js-src(.*)$': '<rootDir>/src$1',
     '\\.(css|less)$': '<rootDir>/buildSrc/__mocks__/styleMock.js'
   },
   setupFilesAfterEnv: ['<rootDir>/buildSrc/setupTest.js'],
