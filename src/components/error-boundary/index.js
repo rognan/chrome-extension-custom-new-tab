03ade5374bef243318831a077d431e9fd75c69f2
Thor Andreas Rognan <thor.rognan@gmail.com>
2021-06-26 23:29:26 +0200

    Drop @sentry/browser from error boundary

--- src/components/error-boundary/index.js
+++ src/components/error-boundary/index.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { init, captureException, lastEventId, showReportDialog } from '@sentry/browser';
-import styles from './index.css';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = {hasError: false};
-    init({
-      dsn: 'https://ae0abaf060a842dd8996a744473193c3@sentry.io/227131'
-    });
   }
-  componentDidCatch(error, info) {
-    this.setState({hasError: true});
-    captureException(error, {extra: info});
+  // eslint-disable-next-line no-unused-vars
+  static getDerivedStateFromError(ignored) {
+    return { hasError: true };
   }
   render() {
     if (this.state.hasError) {
       return (
-        <div onClick={() => lastEventId() && showReportDialog()}>
           <p>Whoops - Something went wrong!</p>
-          <p className={styles.error__clickme}>Click here to fill out a bug report to help us fix this problem faster.</p>
-        </div>
       );
     }
     else {
